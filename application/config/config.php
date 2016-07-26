1ed9110a63376a989f4d43afe21e6466e38f32b7
otnielyehezkiel <striketokill@gmail.com>
2016-07-26 17:50:17 +0700

    ad

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/banyuwangi';
+$config['base_url']	= 'http://198.71.80.189:8081';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'artcak123!';
 $db['default']['database'] = 'banyuwangi';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
